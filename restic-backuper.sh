625bd829d14eb31a6748de3ec115d72f58eed2e4
dcrystalj <dcrystalj@gmail.com>
2023-07-06 19:45:36 +0200

    Wait until compose down is properly down

--- restic-backuper.sh
+++ restic-backuper.sh
@@ -9,6 +9,13 @@ for APP in "${APPS[@]}"; do
     $UMBREL_ROOT/scripts/app stop $APP
 done
 
+# Loop until the service is actually stopped
+for APP in "${APPS[@]}"; do
+    while [[ "$($UMBREL_ROOT/scripts/app compose $APP ps -q)" ]]; do
+        sleep 1
+    done
+done
+
 GOMAXPROCS=2 restic -p $INSTALL_DIR/password -r sftp:$SFTP backup $BACKUP_DIR
 GOMAXPROCS=2 restic -p $INSTALL_DIR/password -r sftp:$SFTP forget --keep-daily 2 --keep-monthly 1 --prune
 
